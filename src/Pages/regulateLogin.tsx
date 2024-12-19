026d971dc5dc003fbe08f1deebbb33e7008d2bbd
Ashiru Olanrewaju <lanre2967@gmail.com>
2024-12-19 23:23:51 +0100

    fixed route restriction bug

--- src/Pages/regulateLogin.tsx
+++ src/Pages/regulateLogin.tsx
@@ -6,17 +6,24 @@ import { toast } from "react-toastify";
 const UseCheckUserSession: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
     const { user, setUser } = useUser();
     const navigate = useNavigate();
+    const location = window.location;
 
     useEffect(() => {
-        if (!user && !localStorage.getItem("user")) {
-            navigate("/login");
+        if (!user && !localStorage.getItem("user") && (location.pathname != "/login" || location.pathname as string != "/signup")) {
+            if(location.pathname == '/login'){
+                navigate('/login')
+            }
+            else if(location.pathname == '/signup'){
+                navigate('/signup')
+            }
+            else{
+                navigate("/login");
+            }
             toast.error("Your session has expired. Please log in again.");
         } else {
             const user_: any = user || JSON.parse(localStorage.getItem("user") || "{}");
             setUser(user_);
-
-            // redirect to home if trying to login when logged in
-            const location = window.location;
+            
             if(location.pathname === "/login" || location.pathname === "/signup"){
                 navigate('/home');
             }
