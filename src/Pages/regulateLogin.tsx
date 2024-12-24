dd38fc430e1389b019563561bd071233cf494868
Ashiru Olanrewaju <lanre2967@gmail.com>
2024-12-24 17:11:05 +0100

    fixed home routing bug

--- src/Pages/regulateLogin.tsx
+++ src/Pages/regulateLogin.tsx
@@ -15,14 +15,17 @@ const UseCheckUserSession: React.FC<{ children?: React.ReactNode }> = ({ childre
             else if(location.pathname == '/signup'){
                 navigate('/signup')
             }
+            else if(location.pathname == '/'){
+                navigate('/')
+            }
             else{
-                navigate("/login");
+                navigate("/");
             }
         } else {
             const user_: any = JSON.parse(localStorage.getItem("user") as string);
             setUser(user_);
             
-            if(location.pathname === "/login" || location.pathname === "/signup"){
+            if(location.pathname === "/login" || location.pathname === "/signup" || location.pathname === "/"){
                 navigate('/home');
             }
         }
